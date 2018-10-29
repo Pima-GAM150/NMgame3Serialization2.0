8f88ef31cad9455ff315cc63e1612cdbfb4100a0
Nate Mendoza <nmendoza82rg@aol.com>
2018-10-29 12:07:26 -0700

    2.1 Menu Update
    Added a screen manager to the game so now you can switch screens with the escape key or through the UI buttons that are marked accordingly

---
+++ Assets/MyScripts/ScreenSelection.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenSelection : MonoBehaviour {
+
+    public int mainMenuIndex = 0;
+    public int playerMetalScreen = 1;
+    
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                SceneManager.LoadScene(playerMetalScreen);
+            }
+
+            if (gameObject == null)
+            {
+                SceneManager.LoadScene(playerMetalScreen);
+            }
+
+
+
+
+    }
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene(playerMetalScreen);
+
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(mainMenuIndex);
+    }
+
+}
+
+
