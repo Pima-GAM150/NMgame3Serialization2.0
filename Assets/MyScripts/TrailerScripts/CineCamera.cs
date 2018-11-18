3e3972bdbc4835cd71cff01c56847b055d429f16
Nate Mendoza <nmendoza82rg@aol.com>
2018-11-18 12:20:17 -0700

    Attempt to merge the new work on the metal page and the saveload page
    this was s o dumb as soon as i take in the files from drive the freaking commits showed up

---
+++ Assets/MyScripts/TrailerScripts/CineCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CineCamera : MonoBehaviour {
+
+    Camera myCamera;
+    Vector3 cameraPos;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        myCamera = FindObjectOfType<Camera>();
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myCamera.transform.Translate(0, 0, -speed * Time.deltaTime);
+	}
+}
