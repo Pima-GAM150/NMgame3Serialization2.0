528373d7bdaabf05ed4f6d057ad3d6a53b653074
Nate Mendoza <nmendoza82rg@aol.com>
2018-11-18 16:55:39 -0700

    Officially off to a new start
    Began building a shooter mini game after
    got an overlay, sprites to use and getting some tricks down.
    finally all things are working with the music selection pieces, need to adjust volume and fixe volume with drew

---
+++ Assets/MyScripts/ShooterScripts/BulletShot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletShot : MonoBehaviour {
+
+    public int secondsTilDeath;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(DestroyAfterSeconds(secondsTilDeath));
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    IEnumerator DestroyAfterSeconds(int seconds)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(seconds);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/MyScripts/ShooterScripts/PlayerScripts.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScripts : MonoBehaviour {
+
+    
+    public float speed;
+    public BulletShot projectile;
+    public Transform barrel;
+
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerMovement();
+
+        PlayerShoot();
+	}//end update
+
+   
+
+
+
+
+    void PlayerShoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("Firing with space");
+            Rigidbody2D clone;
+            clone = Instantiate(projectile.GetComponent<Rigidbody2D>(), transform.position, transform.rotation);
+            clone.velocity = transform.TransformDirection(Vector3.up * 10);
+        }
+    }
+
+
+    void PlayerMovement()
+    {
+        float horiz = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        transform.Translate(new Vector2(horiz, 0f));
+    }
+
+
+
+}
